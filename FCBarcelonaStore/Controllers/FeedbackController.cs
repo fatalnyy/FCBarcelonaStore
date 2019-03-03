714857a8530222f975557e6b98fe6bb078a4019a
unknown <adam.penczek11@gmail.com>
2019-03-03 20:24:24 +0100

    Added login capabilities via asp net core identity

--- FCBarcelonaStore/Controllers/FeedbackController.cs
+++ FCBarcelonaStore/Controllers/FeedbackController.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using FCBarcelonaStore.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace FCBarcelonaStore.ViewModels
 {
+    [Authorize]
     public class FeedbackController : Controller
     {
         private IFeedbackRepository _feedbackRepository;
--- FCBarcelonaStore/Startup.cs
+++ FCBarcelonaStore/Startup.cs
@@ -38,12 +38,12 @@ namespace FCBarcelonaStore
             app.UseDeveloperExceptionPage();
             app.UseStatusCodePages();
             app.UseStaticFiles();
+            app.UseAuthentication();
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}")
-                    ;
+                    template: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
